72ff396741b77c071a6cb342d93e0a9ab1797e89
David Noren <dcnoren@gmail.com>
2016-02-16 14:06:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ client.on('connect', function() { // When connected
       var idx = jsonobj.idx;
       console.log("idx " + idx);
       if (jsonobj.dtype === "Light/Switch") {
+           var id = idx.toString();
            io.emit('push',
             {
                 "lights":{
-                  'jsonobj.idx':{
+                  id:{
                      "Status":"On",
                      "Level":100,
                      "Type":"Light\/Switch",
