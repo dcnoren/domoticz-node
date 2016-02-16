d02ca22b2971446fab3657856cd6238f40e54d8e
David Noren <dcnoren@gmail.com>
2016-02-16 15:12:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.on('connect', function() { // When connected
            
            annoying = {
                    "lights":{
-                      "[jsonobj.idx].toString()":{
+                      [jsonobj.idx].toString():{
                          "Status":"On",
                          "Level":100,
                          "Type":"Light\/Switch",
