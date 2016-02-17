b58d35e0d53675b3f15baadb1456bbbf3497dda8
David Noren <dcnoren@gmail.com>
2016-02-17 00:59:09 -0500

    a
    a

--- static/js/functions.js
+++ static/js/functions.js
@@ -172,15 +172,15 @@ $(document).ready(function(){
                         // set up the updating of the chart each second
                         var series = this.series[0];
                         setInterval(function () {
-                            var x = 1, // current time
-                                y = 51;
+                            var x = (new Date()).getTime(), // current time
+                                y = Math.random();
                             series.addPoint([x, y], true, true);
                         }, 1000);
                     }
                 }
             },
             title: {
-                text: 'Temperature'
+                text: 'Live random data'
             },
             xAxis: {
                 type: 'datetime',
@@ -188,13 +188,20 @@ $(document).ready(function(){
             },
             yAxis: {
                 title: {
-                    text: 'Fahrenheit'
+                    text: 'Value'
                 },
                 plotLines: [{
-                    value: 32,
+                    value: 0,
                     width: 1,
                     color: '#808080'
                 }]
+            },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                        Highcharts.numberFormat(this.y, 2);
+                }
             },
             legend: {
                 enabled: false
@@ -202,16 +209,23 @@ $(document).ready(function(){
             exporting: {
                 enabled: false
             },
-            series: [
-					{
-					x: 1,
-					y: 52,
-					},
-					{
-					x: 1,
-					y: 49,
-					}
-					]
+            series: [{
+                name: 'Random data',
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -19; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.random()
+                        });
+                    }
+                    return data;
+                }())
+            }]
         });
 	
 	
