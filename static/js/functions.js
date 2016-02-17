438420d4977a09f9404de21bbbe2b5584d2abf6f
David Noren <dcnoren@gmail.com>
2016-02-17 01:14:23 -0500

    a
    a

--- static/js/functions.js
+++ static/js/functions.js
@@ -210,7 +210,10 @@ $(document).ready(function(){
                 enabled: false
             },
             series: [{
-            name: 'Tokyo',
+            name: 'Winter 2012-2013',
+            // Define the data points. All series have a dummy year
+            // of 1970/71 in order to be compared on the same x axis. Note
+            // that in JavaScript, months start at 0 for January, 1 for February etc.
             data: [
                 [Date.UTC(1970, 9, 21), 0],
                 [Date.UTC(1970, 10, 4), 0.28],
@@ -230,6 +233,49 @@ $(document).ready(function(){
                 [Date.UTC(1971, 4, 5), 2.22],
                 [Date.UTC(1971, 4, 19), 1.15],
                 [Date.UTC(1971, 5, 3), 0]
+            ]
+        }, {
+            name: 'Winter 2013-2014',
+            data: [
+                [Date.UTC(1970, 9, 29), 0],
+                [Date.UTC(1970, 10, 9), 0.4],
+                [Date.UTC(1970, 11, 1), 0.25],
+                [Date.UTC(1971, 0, 1), 1.66],
+                [Date.UTC(1971, 0, 10), 1.8],
+                [Date.UTC(1971, 1, 19), 1.76],
+                [Date.UTC(1971, 2, 25), 2.62],
+                [Date.UTC(1971, 3, 19), 2.41],
+                [Date.UTC(1971, 3, 30), 2.05],
+                [Date.UTC(1971, 4, 14), 1.7],
+                [Date.UTC(1971, 4, 24), 1.1],
+                [Date.UTC(1971, 5, 10), 0]
+            ]
+        }, {
+            name: 'Winter 2014-2015',
+            data: [
+                [Date.UTC(1970, 10, 25), 0],
+                [Date.UTC(1970, 11, 6), 0.25],
+                [Date.UTC(1970, 11, 20), 1.41],
+                [Date.UTC(1970, 11, 25), 1.64],
+                [Date.UTC(1971, 0, 4), 1.6],
+                [Date.UTC(1971, 0, 17), 2.55],
+                [Date.UTC(1971, 0, 24), 2.62],
+                [Date.UTC(1971, 1, 4), 2.5],
+                [Date.UTC(1971, 1, 14), 2.42],
+                [Date.UTC(1971, 2, 6), 2.74],
+                [Date.UTC(1971, 2, 14), 2.62],
+                [Date.UTC(1971, 2, 24), 2.6],
+                [Date.UTC(1971, 3, 2), 2.81],
+                [Date.UTC(1971, 3, 12), 2.63],
+                [Date.UTC(1971, 3, 28), 2.77],
+                [Date.UTC(1971, 4, 5), 2.68],
+                [Date.UTC(1971, 4, 10), 2.56],
+                [Date.UTC(1971, 4, 15), 2.39],
+                [Date.UTC(1971, 4, 20), 2.3],
+                [Date.UTC(1971, 5, 5), 2],
+                [Date.UTC(1971, 5, 10), 1.85],
+                [Date.UTC(1971, 5, 15), 1.49],
+                [Date.UTC(1971, 5, 23), 1.08]
             ]
         }]
         });
