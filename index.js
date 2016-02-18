7c6d0140b9b98a93c62ef3b9fe71355e6dfd0b0a
David Noren <dcnoren@gmail.com>
2016-02-18 13:42:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var mqttOptions = {
 
 //Specify the items you care about here
 var dimmers = [7, 8, 9, 10, 34, 36, 38, 47, 48];
-var fans = [37];
+var fans = [37, 22]; //22 is tempattic
 var doors = [28, 29, 30];
 //var scenes = [1, 2, 3, 4, 5, 6, 7]; //These aren't real IDXes, so can't do this...
 
