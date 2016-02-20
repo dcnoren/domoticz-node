fef34dd620850f49f9130f5e4b0b0ae5b9af1d5b
David Noren <dcnoren@gmail.com>
2016-02-20 16:57:02 -0500

    Update functions.js

--- static/js/functions.js
+++ static/js/functions.js
@@ -215,6 +215,12 @@ $(document).ready(function(){
                     color: '#808080'
                 }]
             },
+            legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+            },
             tooltip: {
                 formatter: function () {
                     return '<b>' + this.series.name + '</b><br/>' +
