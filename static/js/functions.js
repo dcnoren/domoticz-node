5f8666bd46452fe7ec1a154dbd50296bfec870e3
David Noren <dcnoren@gmail.com>
2016-02-20 17:19:26 -0500

    Update functions.js

--- static/js/functions.js
+++ static/js/functions.js
@@ -167,7 +167,7 @@ $(document).ready(function(){
                 type: 'spline',
                 renderTo: 'chartsBoard',
                 animation: Highcharts.svg, // don't animate in old IE
-                marginBottom: 30,
+                marginBottom: 150,
                 events: {
                     load: function () {
 			socket.on('chart', function (sample) {
