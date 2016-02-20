139757a13da8cbf7f283a5ef9e9d078965b5050e
David Noren <dcnoren@gmail.com>
2016-02-20 14:57:30 -0500

    Update functions.js

--- static/js/functions.js
+++ static/js/functions.js
@@ -173,8 +173,8 @@ $(document).ready(function(){
                         // when a sample arrives we plot it
                         series.addPoint([sample.x, sample.y], true, true);
                         Highcharts.charts[0].xAxis[0].update({
-    				min: new Date((sample.x - 3600) * 1000).getTime(),
-                		max: new Date(sample.x * 1000).getTime()
+    				min: new Date(sample.x - 3600).getTime(),
+                		max: new Date(sample.x).getTime()
 			});
 						});
                     }
