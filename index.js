bead4a06af36914f938385cddca3845517307662
David Noren <dcnoren@gmail.com>
2016-02-20 19:08:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,14 @@ var params = {
 	}
 }
 
-docClient.put(params);
+console.log("Adding a new item...");
+docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+});
 
 app.use(express.static('static'));
 
