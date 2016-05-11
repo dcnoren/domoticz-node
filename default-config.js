2d394790cd3255ecbcd2f0564f5c8a21930bb210
David Noren <dcnoren@gmail.com>
2016-05-11 19:24:36 -0400

    a
    a

--- default-config.js
+++ default-config.js
@@ -5,9 +5,9 @@ config.mqtt = {};
 
 config.wit.key = process.env.TWITTER_USER || 'username';
 config.wit.password=  process.env.TWITTER_PASSWORD || 'password';
-config.mqtt.host = process.env.MQTT_HOST || 'm10.cloudmqtt.com';
-config.mqtt.port = process.env.MQTT_PORT || '12556';
-config.mqtt.username = process.env.MQTT_USERNAME || 'soaring';
-config.mqtt.password=  process.env.MQTT_PASSWORD || 'password';
+config.mqtt.host = process.env.MQTT_HOST || 'MQTT HOST';
+config.mqtt.port = process.env.MQTT_PORT || 'MQTT PORT';
+config.mqtt.username = process.env.MQTT_USERNAME || 'MQTT USERNAME';
+config.mqtt.password=  process.env.MQTT_PASSWORD || 'MQTT PASSWORD';
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var config = require('./default-config.js');
+var config = require('./config.js');
 
 var express = require('express');
 var app = express();
