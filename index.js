b2407ce1cd4dec84bafc15733c745c975b371cb3
David Noren <dcnoren@gmail.com>
2016-05-11 21:29:24 -0400

    a

--- index.js
+++ index.js
@@ -276,7 +276,7 @@ io.on('connection', function(socket){
 
       myCommand = '{"command": "switchlight", "idx": ' + device + ', "switchcmd": "Off", "level": 0 }';
       console.log('message: ' + myCommand);
-      mqttClient.publish('domoticz/in', myCommand);
+      //mqttClient.publish('domoticz/in', myCommand);
     }
 
 
