9babd4fcc4340c6a02c958b5f7b3fb3c8156c504
David Noren <dcnoren@gmail.com>
2016-05-11 22:39:50 -0400

    a

--- static/js/functions.js
+++ static/js/functions.js
@@ -253,52 +253,52 @@ $(document).ready(function(){
 
         //MICROPHONE
           var mic = new Wit.Microphone(document.getElementById("microphone"));
-          var info = function (msg) {
-            document.getElementById("info").innerHTML = msg;
-          };
-          var error = function (msg) {
-            document.getElementById("error").innerHTML = msg;
-          };
-          mic.onready = function () {
-            info("Microphone is ready to record");
-          };
-          mic.onaudiostart = function () {
-            info("Recording started");
-            error("");
-          };
-          mic.onaudioend = function () {
-            info("Recording stopped, processing started");
-          };
+          // var info = function (msg) {
+          //   document.getElementById("info").innerHTML = msg;
+          // };
+          // var error = function (msg) {
+          //   document.getElementById("error").innerHTML = msg;
+          // };
+          // mic.onready = function () {
+          //   info("Microphone is ready to record");
+          // };
+          // mic.onaudiostart = function () {
+          //   info("Recording started");
+          //   error("");
+          // };
+          // mic.onaudioend = function () {
+          //   info("Recording stopped, processing started");
+          // };
           mic.onresult = function (intent, entities, a) {
-            var r = kv("intent", intent);
-
-            for (var k in entities) {
-              var e = entities[k];
-
-              if (!(e instanceof Array)) {
-                r += kv(k, e.value);
-              } else {
-                for (var i = 0; i < e.length; i++) {
-                  r += kv(k, e[i].value);
-                }
-              }
-            }
-
-            document.getElementById("result").innerHTML = r;
+            // var r = kv("intent", intent);
+            //
+            // for (var k in entities) {
+            //   var e = entities[k];
+            //
+            //   if (!(e instanceof Array)) {
+            //     r += kv(k, e.value);
+            //   } else {
+            //     for (var i = 0; i < e.length; i++) {
+            //       r += kv(k, e[i].value);
+            //     }
+            //   }
+            // }
+            //
+            // document.getElementById("result").innerHTML = r;
 
             socket.emit('voice', a);
 
 
           };
-          mic.onerror = function (err) {
-            error("Error: " + err);
-          };
-          mic.onconnecting = function () {
-            info("Microphone is connecting");
-          };
-          mic.ondisconnected = function () {
-            info("Microphone is not connected");
-          };
+          // mic.onerror = function (err) {
+          //   error("Error: " + err);
+          // };
+          // mic.onconnecting = function () {
+          //   info("Microphone is connecting");
+          // };
+          // mic.ondisconnected = function () {
+          //   info("Microphone is not connected");
+          // };
 
           mic.connect("3RKVDARFNBKXOPAHP5B6Z2P5GAMDGNJE");
           // mic.start();
