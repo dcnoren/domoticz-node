179ecc5142e1dabaf826ae9e722174a65e208d94
David Noren <dcnoren@gmail.com>
2016-06-13 19:53:46 -0400

    cleanup

--- app/config.js
+++ app/config.js
@@ -2,11 +2,16 @@ var config = {};
 
 config.wit = {};
 config.mqtt = {};
+config.security = {};
 
-config.wit.key = process.env.WIT_KEY || 'username';
+config.wit.key = '3RKVDARFNBKXOPAHP5B6Z2P5GAMDGNJE';
+
 config.mqtt.host = 'm10.cloudmqtt.com';
 config.mqtt.port = '12556';
 config.mqtt.username = 'soaring';
 config.mqtt.password = 'password';
+
+config.security.username = 'noren';
+config.security.password = '0830';
 
 module.exports = config;
