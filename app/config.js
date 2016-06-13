ede344d785a78cfa4c68f5f94702e72cb9cd1efb
David Noren <dcnoren@gmail.com>
2016-06-13 19:47:50 -0400

    cleanup

--- app/config.js
+++ app/config.js
@@ -4,9 +4,9 @@ config.wit = {};
 config.mqtt = {};
 
 config.wit.key = process.env.WIT_KEY || 'username';
-config.mqtt.host = process.env.MQTT_HOST || 'm10.cloudmqtt.com';
-config.mqtt.port = process.env.MQTT_PORT || '12556';
-config.mqtt.username = process.env.MQTT_USERNAME || 'soaring';
-config.mqtt.password=  process.env.MQTT_PASSWORD || 'password';
+config.mqtt.host = 'm10.cloudmqtt.com';
+config.mqtt.port = '12556';
+config.mqtt.username = 'soaring';
+config.mqtt.password = 'password';
 
 module.exports = config;
