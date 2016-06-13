67d53947e8ece3951aee02a78977b9b55a6560c5
David Noren <dcnoren@gmail.com>
2016-06-13 19:46:30 -0400

    abc

--- app/config.js
+++ app/config.js
@@ -3,8 +3,7 @@ var config = {};
 config.wit = {};
 config.mqtt = {};
 
-config.wit.key = process.env.TWITTER_USER || 'username';
-config.wit.password=  process.env.TWITTER_PASSWORD || 'password';
+config.wit.key = process.env.WIT_KEY || 'username';
 config.mqtt.host = process.env.MQTT_HOST || 'm10.cloudmqtt.com';
 config.mqtt.port = process.env.MQTT_PORT || '12556';
 config.mqtt.username = process.env.MQTT_USERNAME || 'soaring';
