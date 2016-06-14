e8f6aa468a3404ad2d1f951ebc66000135212f05
David Noren <dcnoren@gmail.com>
2016-06-14 18:24:48 -0400

    condensing commits

---
+++ app/app.js
@@ -0,0 +1,347 @@
+var config = require('./config.js');
+var idxMap = require('./idxMap.js');
+
+var express = require('express');
+var app = express();
+
+if(process.env.DOMOTICZ_PORT) {
+    var runPort = process.env.DOMOTICZ_PORT;
+}
+else {
+    var runPort = 3000;
+}
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mqtt = require('mqtt'), url = require('url');
+var AWS = require("aws-sdk");
+var basicAuth = require('basic-auth');
+
+authy = function(username, password) {
+  return function(req, res, next) {
+    var user = basicAuth(req);
+
+    if (!user || user.name !== username || user.pass !== password) {
+      res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+      return res.sendStatus(401);
+    }
+
+    next();
+  };
+};
+
+var router = express.Router();
+
+app.use('/api', router);
+
+router.get('/', function(req, res) {
+    res.json({ message: 'domiticz-node api' });
+});
+
+router.get('/all/off', function(req, res) {
+    res.json({ message: '200 OK' });
+    myCommand = '{"command": "switchscene", "idx": 2, "switchcmd": "On" }';
+    mqttClient.publish('domoticz/in', myCommand);
+});
+
+app.use(express.static('static'));
+
+app.get('/', authy(config.security.username, config.security.password), function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var mqttOptions = {
+    host: config.mqtt.host,
+    port: config.mqtt.port,
+    username: config.mqtt.username,
+    password: config.mqtt.password
+};
+
+
+
+
+// LIGHT FUNCTIONS //
+
+function dimLights(msg){
+
+  //console.log('message: ' + msg);
+  mqttClient.publish('domoticz/in', msg);
+
+  setTimeout(function(str1) {
+    mqttClient.publish('domoticz/in', str1);
+  }, 2000, msg);
+
+  setTimeout(function(str1) {
+    mqttClient.publish('domoticz/in', str1);
+  }, 3000, msg);
+
+  setTimeout(function(str1) {
+    mqttClient.publish('domoticz/in', str1);
+  }, 3500, msg);
+
+}
+
+function pollLights(msg){
+
+  //console.log('message: ' + msg);
+  mqttClient.publish('domoticz/in', msg);
+
+  setTimeout(function(str1) {
+    mqttClient.publish('domoticz/in', str1);
+  }, 500, msg);
+
+  setTimeout(function(str1) {
+    mqttClient.publish('domoticz/in', str1);
+  }, 1000, msg);
+
+  setTimeout(function(str1) {
+    mqttClient.publish('domoticz/in', str1);
+  }, 1500, msg);
+
+  setTimeout(function(str1) {
+    mqttClient.publish('domoticz/in', str1);
+  }, 2500, msg);
+
+  setTimeout(function(str1) {
+    mqttClient.publish('domoticz/in', str1);
+  }, 4000, msg);
+
+}
+
+function switchLights(msg){
+
+  //console.log('message: ' + msg);
+  mqttClient.publish('domoticz/in', msg);
+
+}
+
+
+
+
+// Create a client connection
+var mqttClient = mqtt.connect(mqttOptions);
+
+mqttClient.on('connect', function() {
+    // subscribe to a topic
+    mqttClient.subscribe('domoticz/out', function() {
+        // when a message arrives, do something with it
+        mqttClient.on('message', function(topic, message, packet) {
+            //console.log("Received '" + message + "' on '" + topic + "'");
+            var jsonobj = JSON.parse(message);
+            var idx = jsonobj.idx;
+            var idxname = jsonobj.name;
+            var status = jsonobj.nvalue;
+            var level = jsonobj.svalue1;
+
+            if (jsonobj.switchType === "Dimmer") {
+
+                var cstatus = "";
+
+                if (status === 0){
+                    cstatus = "Off";
+                }
+
+                if (status === 1){
+                    cstatus = "On";
+                }
+
+                if (status === 2 && level != 100){
+                    cstatus = "Transition";
+                }
+
+                if (status === 2 && level === 100){
+                    cstatus = "On";
+                }
+
+
+                var abcdef = "";
+                var abcdef = '{"lights":{"' + idx + '":{"Status":"' + cstatus + '","Level":' + level + ',"Type":"Light\/Switch","Name":"' + idxname + '"}}}';
+                var jsonABC = JSON.parse(abcdef);
+                io.emit('update',jsonABC);
+            }
+
+
+			if (jsonobj.dtype === "Light/Switch" && jsonobj.switchType === "On/Off") {
+
+                var cstatus = "";
+
+                if (status === 0){
+                    cstatus = "Off";
+                }
+
+                if (status === 1){
+                    cstatus = "On";
+                }
+
+                var abcdef = "";
+                var abcdef = '{"fans":{"' + idx + '":{"Status":"' + cstatus + '","Name":"' + idxname + '"}}}';
+                var jsonABC = JSON.parse(abcdef);
+                io.emit('update',jsonABC);
+            }
+
+
+			if (jsonobj.switchType === "Contact") {
+
+                var cstatus = "";
+
+                if (status === 0){
+                    cstatus = "Closed";
+                }
+
+                if (status === 1){
+                    cstatus = "Open";
+                    io.emit('audio');
+                }
+
+                var abcdef = "";
+                var abcdef = '{"doors":{"' + idx + '":{"Status":"' + cstatus + '","Name":"' + idxname + '"}}}';
+                var jsonABC = JSON.parse(abcdef);
+                io.emit('update',jsonABC);
+            }
+
+
+
+      if (jsonobj.dtype === "Scene") {
+
+                cstatus = "Deactivated";
+
+                var abcdef = "";
+                var abcdef = '{"scenes":{"' + idx + '":{"Status":"' + cstatus + '","Name":"' + idxname + '"}}}';
+                var jsonABC = JSON.parse(abcdef);
+                io.emit('update',jsonABC);
+      }
+
+
+
+			if ((jsonobj.dtype === "Temp" || jsonobj.dtype === "Temp + Humidity" || jsonobj.dtype === "Thermostat") && idxname != "House Temperature Setpoint") {
+
+				var x = new Date().getTime();
+
+		                var f = parseFloat(level);
+		                var f = f * 9 / 5 + 32;
+		                f = f.toFixed(1);
+		                f = parseFloat(f);
+
+				io.emit('chart', {
+						x: x,
+						y: f,
+						idx: idx,
+						idxname: idxname
+					});
+
+				//console.log('charted x: ' + x + ' y: ' + f);
+
+            }
+
+        });
+    });
+});
+
+io.on('connection', function(socket){
+
+  io.emit('wit', config.wit.key);
+
+  idxMap.dimmers.items.forEach(function(item) {
+		myCommand = '{"command": "getdeviceinfo", "idx": ' + item + ' }';
+		mqttClient.publish('domoticz/in', myCommand);
+	});
+
+	idxMap.doors.items.forEach(function(item) {
+		myCommand = '{"command": "getdeviceinfo", "idx": ' + item + ' }';
+		mqttClient.publish('domoticz/in', myCommand);
+	});
+
+	idxMap.fans.items.forEach(function(item) {
+		myCommand = '{"command": "getdeviceinfo", "idx": ' + item + ' }';
+		mqttClient.publish('domoticz/in', myCommand);
+	});
+
+	idxMap.temps.items.forEach(function(item) {
+		myCommand = '{"command": "getdeviceinfo", "idx": ' + item + ' }';
+		mqttClient.publish('domoticz/in', myCommand);
+	});
+
+for (var k in idxMap.scenes.definitions){
+    myCommand = "{\"idx\" : " + k + ", \"dtype\" : \"Scene\", \"name\" : \"" + idxMap.scenes.definitions[k] + "\"}";
+		mqttClient.publish('domoticz/out', myCommand);
+}
+
+
+	socket.on('dimCommand', function(msg){
+
+		dimLights(msg);
+
+	});
+
+	socket.on('dimPoll', function(msg){
+
+    pollLights(msg);
+
+	});
+
+	socket.on('switchCommand', function(msg){
+
+		switchLights(msg);
+
+	});
+
+  socket.on('voice', function(msg){
+
+    if (msg.outcome.intent == "light_on_off") {
+
+      devices = {
+        "dining room" : 34,
+        "dining" : 34,
+        "front porch" : 10,
+        "porch" : 10,
+        "garage" : 8,
+        "entry" : 9,
+        "landing" : 38,
+        "family room" : 36,
+        "family" : 36,
+        "downstairs hallway" : 7,
+        "hallway" : 7,
+        "breakfast room" : 47,
+        "breakfast" : 47,
+        "upstairs bathroom" : 40,
+        "bathroom" : 40
+      }
+
+      //console.log(msg.outcome.entities.room.value);
+
+      var roomName = msg.outcome.entities.room.value;
+      var binaryAction = msg.outcome.entities.binarySwitch.value;
+      //console.log(binaryAction);
+      var device = devices[roomName];
+
+      if (binaryAction == "on") {
+        myCommand = '{"command": "switchlight", "idx": ' + device + ', "switchcmd": "On", "level": 100 }';
+        dimLights(myCommand);
+      }
+
+      if (binaryAction == "off") {
+        myCommand = '{"command": "switchlight", "idx": ' + device + ', "switchcmd": "Off", "level": 0 }';
+        dimLights(myCommand);
+      }
+
+    }
+
+
+
+
+
+
+    //mqttClient.publish('domoticz/in', msg);
+
+	});
+
+
+
+
+});
+
+
+
+http.listen(runPort, function(){
+  console.log('listening on *:' + runPort);
+});
---
+++ app/static/js/functions.js
@@ -0,0 +1,331 @@
+var socket = io.connect();
+
+$(document).ready(function(){
+
+      socket.on('update', function(data){
+
+				var lightItems = [];
+
+				if (data.lights){
+					$.each(data.lights, function(key, val) {
+
+						if ($('#lightBoard').find("#" + key + "").length>0) {
+							 lightItems.push('<div id="' + key + '" class="light ui-bar ui-bar-a ' + val.Status + '" style="height:80px"><center><h1>' + val.Name + '</h1></center></div>');
+							 $("#" + key + "").replaceWith(lightItems).enhanceWithin();
+						} else {
+							 lightItems.push('<div class="ui-block-b"><div id="' + key + '" class="light ui-bar ui-bar-a ' + val.Status + '" style="height:80px"><center><h1>' + val.Name + '</h1></center></div></div>');
+							 $('#lightBoard').append(lightItems).enhanceWithin();
+						}
+
+					});
+				}
+
+
+				var doorItems = [];
+
+				if (data.doors){
+					$.each(data.doors, function(key, val) {
+
+						if ($('#doorBoard').find("#" + key + "").length>0) {
+							 doorItems.push('<div id="' + key + '" class="door ui-bar ui-bar-a ' + val.Status + '" style="height:80px"><center><h1>' + val.Name + '</h1></center></div>');
+							 $("#" + key + "").replaceWith(doorItems).enhanceWithin();
+						} else {
+							 doorItems.push('<div class="ui-block-b"><div id="' + key + '" class="door ui-bar ui-bar-a ' + val.Status + '" style="height:80px"><center><h1>' + val.Name + '</h1></center></div></div>');
+							 $('#doorBoard').append(doorItems).enhanceWithin();
+						}
+
+					});
+				}
+
+
+				var fanItems = [];
+
+				if (data.fans){
+					$.each(data.fans, function(key, val) {
+
+						if ($('#fanBoard').find("#" + key + "").length>0) {
+							 fanItems.push('<div id="' + key + '" class="fan ui-bar ui-bar-a ' + val.Status + '" style="height:80px"><center><h1>' + val.Name + '</h1></center></div>');
+							 $("#" + key + "").replaceWith(fanItems).enhanceWithin();
+						} else {
+							 fanItems.push('<div class="ui-block-b"><div id="' + key + '" class="fan ui-bar ui-bar-a ' + val.Status + '" style="height:80px"><center><h1>' + val.Name + '</h1></center></div></div>');
+							 $('#fanBoard').append(fanItems).enhanceWithin();
+						}
+
+					});
+				}
+
+
+				var sceneItems = [];
+
+				if (data.scenes){
+					$.each(data.scenes, function(key, val) {
+
+						if ($('#sceneBoard').find("#" + key + "").length>0) {
+							 sceneItems.push('<div id="' + key + '" class="scene ui-bar ui-bar-a ' + val.Status + '" style="height:80px"><center><h1>' + val.Name + '</h1></center></div>');
+							 $("#" + key + "").replaceWith(sceneItems).enhanceWithin();
+						} else {
+               sceneItems.push('<div class="ui-block-b"><div id="' + key + '" class="scene ui-bar ui-bar-a ' + val.Status + '" style="height:80px"><center><h1>' + val.Name + '</h1></center></div></div>');
+							 $('#sceneBoard').append(sceneItems).enhanceWithin();
+						}
+
+					});
+				}
+
+				//ONLY ONE NOT DONE
+				var securityItems = [];
+
+				if (data.security){
+				$.each(data.security, function(key, val) {
+					securityItems.push('<div class="ui-block-b"><div id="' + key + '" class="security ui-bar ui-bar-a ' + val.Status + '" style="height:80px"><center><h1>' + val.Name + '</h1></center></div></div>');
+				});
+				$('#securityBoard').html(securityItems).enhanceWithin();
+				}
+
+
+
+
+      });
+
+
+  socket.on('audio', function(){
+    $("#audioBell").get(0).play();
+  });
+
+
+	$(document).on('click', '.light.Off', function() {
+    $(this).addClass("Transition").removeClass("Off");
+		myidx = $(this).attr("id");
+		var myCommand = "";
+		myCommand = '{"command": "switchlight", "idx": ' + myidx + ', "switchcmd": "On", "level": 100 }';
+		socket.emit('dimCommand', myCommand);
+		//myCommand = '{"command": "getdeviceinfo", "idx": ' + myidx + ' }';
+		//socket.emit('dimPoll', myCommand);
+		return false;
+	});
+
+	$(document).on('click', '.light.On', function() {
+		$(this).addClass("Transition").removeClass("On");
+		myidx = $(this).attr("id");
+		var myCommand = "";
+		myCommand = '{"command": "switchlight", "idx": ' + myidx + ', "switchcmd": "Off", "level": 0 }';
+		socket.emit('dimCommand', myCommand);
+		//myCommand = '{"command": "getdeviceinfo", "idx": ' + myidx + ' }';
+		//socket.emit('dimPoll', myCommand);
+		return false;
+	});
+
+	$(document).on('click', '.light.Transition', function() {
+		$(this).addClass("Transition").removeClass("On");
+		myidx = $(this).attr("id");
+		var myCommand = "";
+		myCommand = '{"command": "switchlight", "idx": ' + myidx + ', "switchcmd": "Off", "level": 0 }';
+		socket.emit('dimCommand', myCommand);
+		//myCommand = '{"command": "getdeviceinfo", "idx": ' + myidx + ' }';
+		//socket.emit('dimPoll', myCommand);
+		return false;
+	});
+
+	$(document).on('click', '.fan.Off', function() {
+		$(this).addClass("Transition").removeClass("Off");
+		myidx = $(this).attr("id");
+		var myCommand = "";
+		myCommand = '{"command": "switchlight", "idx": ' + myidx + ', "switchcmd": "On" }';
+		socket.emit('switchCommand', myCommand);
+		return false;
+	});
+
+	$(document).on('click', '.fan.On', function() {
+		$(this).addClass("Transition").removeClass("On");
+		myidx = $(this).attr("id");
+		var myCommand = "";
+		myCommand = '{"command": "switchlight", "idx": ' + myidx + ', "switchcmd": "Off" }';
+		socket.emit('switchCommand', myCommand);
+		return false;
+	});
+
+	/*$(document).on('click', '.security.disabled', function() {
+		mystatus = $(this).attr("id");
+		$.get('ajax/ajax.php?action=setSecurity&command=' + mystatus);
+		$("#securityBoard").parent().addClass("ui-disabled").delay(30000).queue(function(next){
+			$(this).removeClass("ui-disabled");
+			next();
+		});
+	});*/
+
+	$(document).on('click', '.scene.Deactivated', function() {
+    $(this).siblings(".Activated").removeClass("Activated");
+    $(this).switchClass("Deactivated", "Activated");
+    //$(this).removeClass("Deactivated");
+    setTimeout(function() {
+        //$(this).addClass("Deactivated").clearQueue();
+        $('.scene.Activated').switchClass("Activated", "Deactivated", 5000).clearQueue();
+    },5000);
+    myidx = $(this).attr("id");
+		var myCommand = "";
+		myCommand = '{"command": "switchscene", "idx": ' + myidx + ', "switchcmd": "On" }';
+		socket.emit('dimCommand', myCommand);
+		return false;
+	});
+
+
+
+
+	//$('#chartsBoard').highcharts({
+        fart = new Highcharts.Chart({
+            chart: {
+                type: 'spline',
+                renderTo: 'chartsBoard',
+                animation: Highcharts.svg, // don't animate in old IE
+                marginBottom: 60,
+                events: {
+                    load: function () {
+			socket.on('chart', function (sample) {
+
+                        var series = fart.get(sample.idx);
+                        if (!series){
+                        	//console.log('adding series')
+                        	fart.addSeries({
+                        		id: sample.idx,
+                        		name: sample.idxname,
+                        		data: [null,null]
+                        	});
+                        	//fart.isDirtyLegend = true;
+                        	//fart.isDirtyBox = true;
+                        	//fart.redraw();
+                        }
+                        // when a sample arrives we plot it
+                        series = fart.get(sample.idx);
+                        series.addPoint([sample.x, sample.y], true, false);
+                        //console.log('adding data')
+                        fart.xAxis[0].update({
+    				min: new Date(sample.x - 3600000).getTime(),
+                		max: new Date(sample.x).getTime()
+			});
+						});
+                    }
+                }
+            },
+            title: {
+                text: 'Temperatures'
+            },
+            xAxis: {
+                type: 'datetime',
+                tickPixelInterval: 100,
+                min: new Date().setHours(new Date().getHours()-1),
+                max: new Date().setMinutes(new Date().getMinutes()+2),
+            },
+            yAxis: {
+                title: {
+                    text: 'Temp'
+                },
+		tickInterval: 10,
+            	min: 25,
+            	max: 100,
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            legend: {
+            	align: 'center',
+	        verticalAlign: 'bottom',
+	        layout: 'horizontal'
+	       // x: 0,
+	        //y: 100
+            },
+            plotOptions: {
+            	line: {
+                	dataLabels: {
+                    		enabled: true
+                	},
+        		 enableMouseTracking: false
+	        }
+	    },
+            tooltip: {
+                formatter: function () {
+                    return '<b>' + this.series.name + '</b><br/>' +
+                        Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
+                        Highcharts.numberFormat(this.y, 2);
+                }
+            },
+            exporting: {
+                enabled: false
+            }/*,
+            series: [{
+            name: 'Blank',
+            data: [null,null]
+            }]*/
+        });
+
+
+
+        socket.on('wit', function (token) {
+
+        //MICROPHONE
+          var mic = new Wit.Microphone(document.getElementById("microphone"));
+          var info = function (msg) {
+            document.getElementById("info").innerHTML = msg;
+          };
+          var error = function (msg) {
+            document.getElementById("error").innerHTML = msg;
+          };
+          mic.onready = function () {
+            info("Microphone is ready to record");
+          };
+          mic.onaudiostart = function () {
+            info("Recording started");
+            error("");
+          };
+          mic.onaudioend = function () {
+            info("Recording stopped, processing started");
+          };
+          mic.onresult = function (intent, entities, a) {
+            var r = kv("intent", intent);
+
+            for (var k in entities) {
+              var e = entities[k];
+
+              if (!(e instanceof Array)) {
+                r += kv(k, e.value);
+              } else {
+                for (var i = 0; i < e.length; i++) {
+                  r += kv(k, e[i].value);
+                }
+              }
+            }
+
+            document.getElementById("result").innerHTML = r;
+
+            socket.emit('voice', a);
+
+
+          };
+          mic.onerror = function (err) {
+            error("Error: " + err);
+          };
+          mic.onconnecting = function () {
+            info("Microphone is connecting");
+          };
+          mic.ondisconnected = function () {
+            info("Microphone is not connected");
+          };
+
+
+          mic.connect(token);
+          // mic.start();
+          // mic.stop();
+
+          function kv (k, v) {
+            if (toString.call(v) !== "[object String]") {
+              v = JSON.stringify(v);
+            }
+            return k + "=" + v + "\n";
+          }
+
+        });
+
+
+
+
+});
