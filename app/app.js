3b46f8ec2883ce8ac5017fada7c39405afff352c
David Noren <dcnoren@gmail.com>
2016-06-14 15:52:35 -0400

    a

--- app/app.js
+++ app/app.js
@@ -267,7 +267,8 @@ io.on('connection', function(socket){
 	});*/
 
   idxMap.scenes.newDefinitions.forEach(function(item) {
-		myCommand = '{"idx" : ' + item.idx + ', "dtype" : "Scene", "name" : "' + item.Name '"}';
+		myCommand = '{"idx" : 1, "dtype" : "Scene", "name" : "All Off"}';
+    //myCommand = '{"idx" : ' + item.idx + ', "dtype" : "Scene", "name" : "' + item.Name '"}';
 		mqttClient.publish('domoticz/out', myCommand);
 	});
 
